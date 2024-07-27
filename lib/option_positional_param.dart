7295e5b123f8357d3601800b591dbcd3881dd84e
Mohamed Yansane <yansane472@gmail.com>
2024-07-27 13:06:13 +0000

    adding comment

--- lib/option_positional_param.dart
+++ lib/option_positional_param.dart
@@ -1,7 +1,9 @@
 void main(){
+  //here we give two params cause the name 3 is optional
   printCities(name1: "Sydney", name2: "Dakar");
 }
 
+// here we r passing arguments to our function name1,2 and 3
 void printCities({ required String name1, required String name2, String? name3}){
   print("The city number 1 is $name1");
   print("The city number 2 is $name2");
