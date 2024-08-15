01311e2d3e7e9dd28d721109caa6cd633cafdd42
Mohamed Yansane <yansane472@gmail.com>
2024-08-15 19:05:48 +0000

    adding more exemple to mixin concept

--- lib/mixin.dart
+++ lib/mixin.dart
@@ -2,14 +2,28 @@ void main(){
   Animal animal = Animal();
   animal.jumpFn();
 
+  Cat cat = Cat();
+  cat.fn();
+
 }
 //TODO: this how to use mixin like class
 mixin Jump{
   int jump = 10;
 }
+
+//TODO we can have multiple mixin
+mixin Scream {
+  bool isScreaming = true;
+}
 //TODO when we use mixin the class that inherit from it must use with
-class Animal with Jump{
+class Animal with Jump, Scream{
   void jumpFn(){
-    print('Jumping height is $jump cm');
+    print('Jumping height is $jump cm and screaming ${isScreaming ? 'yes': "no"}');
+  }
+}
+//TODO: notice here i can use or access to jump here because of Animal
+class Cat extends Animal{
+  void fn(){
+    print("jumping height in cm is: $jump ");
   }
 }
