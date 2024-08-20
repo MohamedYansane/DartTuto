3917f9e865716153ae51b2f0a9b7eeb43df62c50
Mohamed Yansane <yansane472@gmail.com>
2024-08-20 23:47:50 +0000

    Set concept

--- lib/sets_concept.dart
+++ lib/sets_concept.dart
@@ -1,6 +1,39 @@
 void main(){
-  List<int> marks = [1,3,5,1,4,5,3];
-  Set<int>m = {};
-  m.add(2);
-  print(m);
+
+
+
+  List<Etudiant>etudiants = [Etudiant('Yansane', 18), Etudiant('Mohamed', 19),
+    Etudiant('Medo', 15), Etudiant('Baba', 10),Etudiant('Alpha', 08)];
+
+}
+
+//TODO generics <datatype> is called generic here we didn't indicate the types we just
+//TODO used T so dart will automatically identified the type when we instancied
+class Student<T>{
+  final T name;
+  Student(this.name);
+
+  //we can also use the T
+  void printName(T name){
+    print('Name is: $name');
+  }
+  //TODO we r using override here with toString cause it dart that create that
+  //TODO by default
+  @override
+  String toString() => 'Student: $name';
+//TODO after overriding the toString now whenever we instancied it won't return
+//TODO Instance of Student
+}
+
+class Animal<String>{
+  final String name;
+  Animal(this.name);
+}
+
+class Etudiant{
+  final String name;
+  final int marks;
+  Etudiant(this.name, this.marks);
+  @override
+  String toString()  => 'Student: $name note $marks';
 }
